f05b516fd4e89e5cae3e5ea5b6d401bdda8ad720
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2022-02-06 20:03:25 +0600

    make it better

--- _token.js
+++ _token.js
@@ -1,10 +1,14 @@
 module.exports = {
     //Put your bot related informations here
-    APP_NAME : "MyPrettyBot",
-    BOT_TOKEN : "MyAwesomeToken",
-    WEBHOOK : 'https://myawesomeurl.whatever', //https://telegram-bot-sdk.readme.io/docs/setwebhook
-    GAME_URL : 'https://mycoolgame.gg',
-    GAME_NAME : 'mycoolgame',
-    PORT : 3000,    // Put your specific port here
-    DEFAULT_PORT : 3000
+    APP_NAME: "MyPrettyBot",
+    BOT_TOKEN: "5293481723:AAEEM1ipEos-ChPwF6I3zSqbOXaOpUtzdMQ",
+    WEBHOOK: 'https://d2a8-5-76-68-201.ngrok.io/', //https://telegram-bot-sdk.readme.io/docs/setwebhook
+    GAME_URL: 'http://192.168.100.3:3000/index.html',
+    GAME_NAME: 'three3ninjas',
+    PORT: 3000, // Put your specific port here
+    DEFAULT_PORT: 3000
 }
+
+//    http://127.0.0.1:4040                                                                                                                                            
+// Forwarding                    http://147a-5-76-68-201.ngrok.io -> http://localhost:3000                                                                                                        
+// Forwarding                    https://147a-5-76-68-201.ngrok.io -> http://localhost:3000

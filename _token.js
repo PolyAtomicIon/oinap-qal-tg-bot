dbc329bcf01528bebcc5de39521b9e1b3a2ac665
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2022-02-06 20:05:49 +0600

    make it better

--- _token.js
+++ _token.js
@@ -2,8 +2,8 @@ module.exports = {
     //Put your bot related informations here
     APP_NAME: "MyPrettyBot",
     BOT_TOKEN: "5293481723:AAEEM1ipEos-ChPwF6I3zSqbOXaOpUtzdMQ",
-    WEBHOOK: 'https://d2a8-5-76-68-201.ngrok.io/', //https://telegram-bot-sdk.readme.io/docs/setwebhook
-    GAME_URL: 'http://192.168.100.3:3000/index.html',
+    WEBHOOK: 'https://biz-oinaimyz.herokuapp.com/', //https://telegram-bot-sdk.readme.io/docs/setwebhook
+    GAME_URL: 'https://biz-oinaimyz.herokuapp.com/index.html',
     GAME_NAME: 'three3ninjas',
     PORT: 3000, // Put your specific port here
     DEFAULT_PORT: 3000
